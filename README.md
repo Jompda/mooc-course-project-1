f5d629570876d9004e9693fece4c749c7cf73703
Joni Rapo <jonirapo@gmail.com>
2024-11-17 15:50:41 +0200

    edistystä

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ patrick:asteroid
 
 ## Vulnerabilities
 
-- [ ] A01:2021-Broken access control
+- [X] A01:2021-Broken access control
   - "Access control enforces policy such that users cannot act outside of their intended permissions." https://owasp.org/Top10/A01_2021-Broken_Access_Control/
     - "Unohda" laittaa login requirement johki viewiin,
 
@@ -21,11 +21,11 @@ patrick:asteroid
   - "Shifting up one position to #2, previously known as Sensitive Data Exposure, which is more of a broad symptom rather than a root cause, the focus is on failures related to cryptography (or lack thereof)" https://owasp.org/Top10/A02_2021-Cryptographic_Failures/
     - lack thereof, voisko olla hashaamattomia saliksia databases -> käykö vulnerability?
 
-- [ ] A03:2021-Injection (SQL ja Cross-Site Scripting)
+- [X] A03:2021-Injection (SQL ja Cross-Site Scripting)
   - "User-supplied data is not validated, filtered, or sanitized by the application." https://owasp.org/Top10/A03_2021-Injection/
     - SQL injektio: kaiva vaikka joku salis tietokannasta
 
-- [ ] A07:2021-Identification and Authentication Failures
+- [X] A07:2021-Identification and Authentication Failures
   - Permits brute force or other automated attacks. https://owasp.org/Top10/A07_2021-Identification_and_Authentication_Failures/
     - Jätä login bruteforcelle alttiiks.
     - hackpassword tiedosto tuotu, TODO siel
--- server/pages/views.py
+++ server/pages/views.py
@@ -5,15 +5,14 @@ from django.contrib.auth.models import User
 from django.db import transaction, connection
 from .models import Account
 
-# old transfer function vulnerable to injection
+# A03:2021-Injection: Vulnerable to SQL injection.
+# Fix by using django orm.
 @transaction.atomic
 def transfer(sender, receiver, amount):
-	# TODO: an example query that exploits this
 	if sender == receiver:
 		return
-	
-	cursor = connection.cursor()
 
+	cursor = connection.cursor()
 	cursor.execute("SELECT balance FROM pages_account WHERE id = %s" % sender)
 	acc1_balance = cursor.fetchone()[0]
 	cursor.execute("SELECT balance FROM pages_account WHERE id = %s" % receiver)
@@ -21,36 +20,66 @@ def transfer(sender, receiver, amount):
 
 	if amount <= 0:
 		return
-	
 	if acc1_balance < amount:
 		return
-	
+
 	cursor.execute("UPDATE pages_account SET balance = '%s' WHERE id = %s" % (acc1_balance - amount, sender))
 	cursor.execute("UPDATE pages_account SET balance = '%s' WHERE id = %s" % (acc2_balance + amount, receiver))
+# END OF A03:2021-Injection
 
+
+# A01:2021-Broken access control: Allows users to performs tasks not in their scope.
+# Fix by using session
+@login_required
+def transferView(request):
+	#request.session['to'] = request.GET.get('to')
+	#request.session['amount'] = int(request.GET.get('amount'))
+	return render(request, 'pages/confirm.html', {
+		'sender': request.user.account.id,
+		'receiver': User.objects.get(username=request.GET.get('to')).account.id,
+		'amount': request.GET.get('amount')
+	})
 
 @login_required
 def confirmView(request):
-	amount = request.session['amount'] # NOTE: Might not be parseable
-	receiver = User.objects.get(username=request.session['to'])
-
-	transfer(request.user.account.id, receiver.account.id, int(amount))
-	request.user.account.refresh_from_db()
-	receiver.account.refresh_from_db()
-	
+	# User-supplied ids
+	# fix by getting ids from request.user and request.session['to']
+	sender_id = request.GET.get('sender')
+	receiver_id = request.GET.get('receiver')
+	amount = int(request.GET.get('amount'))
+	transfer(sender_id, receiver_id, amount)
+	Account.objects.get(id=sender_id).refresh_from_db()
+	Account.objects.get(id=receiver_id).refresh_from_db()
 	return redirect('/')
+# END OF A01:2021-Broken access control
 
+
+# A07:2021-Identification and Authentication Failures: Allows brute-force attacks
+# Fix by keeping track of login attempts by ip-address.
+"""
+Decorator @login_required from django.contrib.auth.decorators
+def login_required(
+    function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url=None
+):
+    #Decorator for views that checks that the user is logged in, redirecting
+    #to the log-in page if necessary.
+
+    actual_decorator = user_passes_test(
+        lambda u: u.is_authenticated,
+        login_url=login_url,
+        redirect_field_name=redirect_field_name,
+    )
+    if function:
+        return actual_decorator(function)
+    return actual_decorator
+"""
 @login_required
-def transferView(request):
-	request.session['to'] = request.GET.get('to')
-	request.session['amount'] = int(request.GET.get('amount'))
-	return render(request, 'pages/confirm.html')
-
-
-@login_required
+# END OF A07:2021-Identification and Authentication Failures
 def homePageView(request):
 	accounts = Account.objects.exclude(user_id=request.user.id)
 	return render(request, 'pages/index.html', {'accounts': accounts})
+
+
 
 # To illustrate CSRF
 def csrfView(request):
