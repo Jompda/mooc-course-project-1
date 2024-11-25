f0799ee2a8b3f5626444cb8d2d4fc34a5d61ddb6
Joni Rapo <jonirapo@gmail.com>
2024-11-25 13:23:19 +0200

    progress

--- server/pages/views.py
+++ server/pages/views.py
@@ -6,37 +6,67 @@ from django.db import transaction, connection
 from .models import Account
 
 
+"""
+A07:2021-Identification and Authentication Failures: Allows brute-force attacks
+The decorator login_required doesn't provide rate-limiting per IP-address, nor account protection via lockdown.
+Fix by keeping track of login attempts by IP-address and setting a hard limit for a certain amount of time.
+Botnets with multiple IP-addresses are still a problem though. To protect an account,
+the limit can also be set for the account itself, causing it to get locked.
+"""
+@login_required
+def homePageView(request):
+	accounts = Account.objects.exclude(user_id=request.user.id)
+	return render(request, 'pages/index.html', {'accounts': accounts})
+
+
 # A01:2021-Broken access control: Allows users to performs tasks not in their scope.
-# Fix by using session
 @login_required
 def transferView(request):
-	#request.session['to'] = request.GET.get('to')
-	#request.session['amount'] = int(request.GET.get('amount'))
-	return render(request, 'pages/confirm.html', {
+	"""
+	Program provides the client a context which contains account ids which the client then sends back to complete the transfer.
+	During this time, the client can manipulate the ids and amount to their will, giving any authenticated user ability to
+	transfer any amount from any account to any account.
+	"""
+	context = {
 		'sender': request.user.account.id,
 		'receiver': User.objects.get(username=request.GET.get('to')).account.id,
 		'amount': request.GET.get('amount')
-	})
-
-@login_required
-def confirmView(request):
-	# User-supplied ids
-	# fix by getting ids from request.user and request.session['to']
+	}
+	"""
+	Fix is not to provide context and to use session to store the transaction details serverside. Access control is taken care of by the login_required
+	decorator because that way request.user gets populated which is then used to determine the sender.
+	Example:
+	request.session['to'] = request.GET.get('to')
+	request.session['amount'] = int(request.GET.get('amount'))
+    """
+	return render(request, 'pages/confirm.html', context)
 
 # A03:2021-Injection
+@login_required
+def confirmView(request):
 	sender_id = request.GET.get('sender')
 	receiver_id = request.GET.get('receiver')
 	amount = int(request.GET.get('amount'))
+
 	# Client-provided content is passed directly to a vulnerable function.
 	transfer(sender_id, receiver_id, amount)
-
 	Account.objects.get(id=sender_id).refresh_from_db()
 	Account.objects.get(id=receiver_id).refresh_from_db()
+
+	""" Fix by bypassing vulnerable function and retrieving transaction details from server state.
+	with transaction.atomic():
+		amount = request.session['amount']
+		to = User.objects.get(username=request.session['to'])
+		request.user.account.balance -= amount
+		to.account.balance += amount
+		request.user.account.save()
+		to.account.save()
+    """
 	return redirect('/')
 # END OF A01:2021-Broken access control
 
 # Vulnerable to SQL injection
-# Injection can happen in any SQL statement.
+# Injection can happen in any SQL statement due to lack of sanitation.
 @transaction.atomic
 def transfer(sender, receiver, amount):
 	if sender == receiver:
@@ -58,33 +88,7 @@ def transfer(sender, receiver, amount):
 # END OF A03:2021-Injection
 
 
-# A07:2021-Identification and Authentication Failures: Allows brute-force attacks
-# Fix by keeping track of login attempts by ip-address.
-"""
-Decorator @login_required from django.contrib.auth.decorators
-def login_required(
-    function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url=None
-):
-    #Decorator for views that checks that the user is logged in, redirecting
-    #to the log-in page if necessary.
 
-    actual_decorator = user_passes_test(
-        lambda u: u.is_authenticated,
-        login_url=login_url,
-        redirect_field_name=redirect_field_name,
-    )
-    if function:
-        return actual_decorator(function)
-    return actual_decorator
-"""
-@login_required
-# END OF A07:2021-Identification and Authentication Failures
-def homePageView(request):
-	accounts = Account.objects.exclude(user_id=request.user.id)
-	return render(request, 'pages/index.html', {'accounts': accounts})
-
-
-
-# To illustrate CSRF
+# To illustrate CSRF vulnerability.
 def csrfView(request):
 	return render(request, 'static/csrf.html')
