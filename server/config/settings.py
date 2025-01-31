7fd9925c51cae66a39af82881618aad8225243e1
Joni Rapo <jonirapo@gmail.com>
2025-01-31 14:49:46 +0200

    specified csrf fix

--- server/config/settings.py
+++ server/config/settings.py
@@ -97,7 +97,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware', # CSRF
+    #'django.middleware.csrf.CsrfViewMiddleware', # UNCOMMENT TO ADD CSRF PROTECTION
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
